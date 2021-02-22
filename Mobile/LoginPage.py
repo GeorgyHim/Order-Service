3a4f55a6ef2300a831926fee0051e5e2152bc022
GeorgyHim <georgyhim1997@yandex.ru>
2021-02-22 19:07:10 +0300

    Rewrite LoginPage

--- Mobile/LoginPage.py
+++ Mobile/LoginPage.py
@@ -1,41 +1,32 @@
 from kivy.uix.screenmanager import Screen
 import json
-
-import utils
 import os.path
 from kivy.clock import Clock
-
-from utils import host, port
+import utils
 
 
 class LoginPage(Screen):
-
     def login(self, HOST, PORT, login):
-        global host, port
-        host, port = HOST, int(PORT)
+        utils.host, utils.port = HOST, int(PORT)
         msg = '{"type":"login","login":"' + login + '"}'
         out = json.loads(utils.send_message(msg))
         print(out)
         if out['result'] == 'true':
-            f = open('login.txt', 'w')
-            f.write(login + ' ' + HOST + ' ' + PORT)
-            global log_name
-            log_name = login
-            f.close()
+            with open('login.txt', 'w') as f:
+                f.write(login + ' ' + HOST + ' ' + PORT)
+            utils.log_name = login
             Clock.schedule_once(self.change_screen)
 
     def on_enter(self):
         Clock.schedule_once(self.change_screen)
 
     def change_screen(self, *args):
-
         if os.path.isfile('login.txt'):
             with open('login.txt', 'r') as f:
-                global log_name, host, port
                 allinfo = f.read().split()
-                log_name = allinfo[0]
-                host = allinfo[1]
-                port = int(allinfo[2])
+                utils.log_name = allinfo[0]
+                utils.host = allinfo[1]
+                utils.port = int(allinfo[2])
 
             self.manager.current = 'main'
     pass
