8bf45752315b5541ea717422229378a15963194e
GeorgyHim <georgyhim1997@yandex.ru>
2021-02-23 20:20:10 +0300

    Add listen() instead of notif, Improve MainPage

--- Mobile/MainPage.py
+++ Mobile/MainPage.py
@@ -1,9 +1,6 @@
 import json
 import os
 from threading import Thread
-from time import sleep
-
-import plyer
 from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.label import Label
@@ -13,34 +10,24 @@ import utils
 
 
 class MainPage(Screen):
-    def notif(self):
+    def on_enter(self):
+        if not utils.listen_working:
+            Thread(target=self.listen).start()
+            utils.listen_working = True
+        Clock.schedule_once(self.change)
+
+    def listen(self):
+        utils.sock.settimeout(None)
         while True:
-
-            msg ='{"operation":"notif", "login":"' + utils.login + '"}'
-            out = json.loads(utils.request_server(msg))
-            if out['result'] == 'true':
-                plyer.notification.notify(title=utils.app_name, message="У вас новые заказы")
-            sleep(30)
-
-    def check_pull_refresh(self, args):
-        if args.scroll_y > 1.03:
-            self.change()
-
-    def logout(self, args):
-        os.remove('login.txt')
-        self.manager.current = 'login'
-
-    def open_order(self, id):
-        utils.id_order = id
-        self.manager.current = 'order'
+            request = utils.sock.recv(4096)
+            utils.process_request(request.replace(b'\x00', b'').decode('utf-8', 'ignore'))
 
     def change(self, *args):
         self.children[0].children[0].clear_widgets()
-        msg ='{"type":"courierOrders","login":"' + utils.login + '"}'
+        data = {'operation': 'get_orders', 'login': utils.login}
+        answer = json.loads(utils.request_server(data))
 
-        json_msg = json.loads(utils.request_server(msg))
-
-        if json_msg['result'] == "fail":
+        if answer['result'] == "fail":
             lbl = Label(text="Связь с сервером отсутсвует",
                         size_hint_y=None,
                         height=100)
@@ -51,7 +38,7 @@ class MainPage(Screen):
                         size_hint_y=None,
                         height=100)
             self.children[0].children[0].add_widget(lbl)
-            for ord in json_msg['result']:
+            for ord in answer['result']:
                 btn = Button(text=ord['address']+' '+ord['start_time'],
                              size_hint_y=None,
                              height=100)
@@ -64,10 +51,16 @@ class MainPage(Screen):
         btn.bind(on_release=self.logout)
         self.children[0].children[0].add_widget(btn)
 
-    def on_enter(self):
-        if utils.f:
-            Thread(target=self.notif).start()
-            f = 0
-        Clock.schedule_once(self.change)
+    def check_pull_refresh(self, args):
+        if args.scroll_y > 1.03:
+            self.change()
+
+    def logout(self, args):
+        os.remove('login.txt')
+        self.manager.current = 'login'
+
+    def open_order(self, id):
+        utils.id_order = id
+        self.manager.current = 'order'
 
     pass
--- Mobile/utils.py
+++ Mobile/utils.py
@@ -1,21 +1,25 @@
 import json
 import socket
 
+import plyer
+
+from MainPage import MainPage
+
 host = ''
 port = ''
 app_name = 'OrderService'
 login = ''
 id_order = 1
-f = 1
+listen_working = False
+sock = socket.socket()
 
 
 def request_server(data):
     try:
         print(data)
         msg = json.dumps(data)
-        sock = socket.socket()
-        sock.settimeout(2)
         sock.connect((host, port))
+        sock.settimeout(2)
         sock.send(msg.encode('UTF-8'))
         answer = sock.recv(4096)
         return answer.replace(b'\x00', b'').decode('utf-8', 'ignore')
@@ -31,3 +35,9 @@ def get_remote_settings():
 
         port_string = f.readline().split()
         port = int(port_string[1])
+
+
+def process_request(request):
+    if request == 'mobile_take_order':
+        plyer.notification.notify(title=app_name, message='У вас новые заказы!')
+        MainPage.change()
