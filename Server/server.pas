a52d3e77a5411657045ded218dd292f2893c66f1
GeorgyHim <georgyhim1997@yandex.ru>
2021-02-23 20:35:25 +0300

    Clean Server code

--- Server/server.pas
+++ Server/server.pas
@@ -165,94 +165,94 @@ begin
 
 
 //    TODO: Убрать это всё
-  if operation = 'courierOrders' then
-    begin
-      courierId := getJsonStringAttribute(receivedJson, 'login').ToInteger();
-      jsonArray := tJsonArray.Create;
-      jsonToSend := tJsonObject.Create;
-      jsonToSend.AddPair('type', 'courierOrders');
-//      dm.qCourierOrders.Close;
-//      dm.qCourierOrders.ParamByName('in_id').Value := courierId;
-//      dm.qCourierOrders.Open;
-//      testString := dm.qCourierOrders.Text;
-//      dm.dsCourierOrders.DataSet := dm.qCourierOrders;
-//      while not dm.dsCourierOrders.DataSet.Eof do
-//      begin
-//        jsonArray.AddElement(tJsonObject.Create);
-//        jsonArrayElement := jsonArray.Items[pred(jsonArray.Count)] as tJsonObject;
-//        jsonArrayElement
-//          .addPair('id', dm.dsCourierOrders.DataSet.FieldByName('id').Value)
-//          .addPair('address', dm.dsCourierOrders.DataSet.FieldByName('address').Value)
-//          .addPair('start_time', DateTimeToStr(dm.dsCourierOrders.DataSet.FieldByName('start_time').Value));
-//        dm.dsCourierOrders.DataSet.Next;
-//      end;
-      jsonToSend.AddPair('result', jsonArray);
-      stringToSend := jsonToSend.ToString;
-      for i := 0 to stringToSend.Length do
-        begin
-           outputByteArray[2*i] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[0];
-          outputByteArray[2*i+1] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[1];
-        end;
-      Socket.SendBuf(outputbyteArray, jsonToSend.ToString.Length*2);
-    end;
-    //Начало добавления
-  if operation = 'courierOrder' then
-    begin
-      orderId := getJsonStringAttribute(receivedJson, 'id').ToInteger();
-      jsonToSend := tJsonObject.Create;
-      jsonToSend.AddPair('type', 'courierOrder');
-//      dm.qCourierOrder.Close;
-//      dm.qCourierOrder.ParamByName('in_order_id').Value := orderId;
-//      dm.qCourierOrder.Open;
-//      testString := '';
-//      dm.dsCourierOrder.DataSet := dm.qCourierOrder;
-//      dm.qCourierOrder_List.Close;
-//      dm.qCourierOrder_List.ParamByName('in_order_id').Value := dm.dsCourierOrder.DataSet.FieldByName('ID').Value;
-//      dm.qCourierOrder_List.Open;
-//      dm.dsCourierOrder_List.DataSet := dm.qCourierOrder_List;
-//      while not dm.dsCourierOrder_List.DataSet.Eof do
-//      begin
-//        testString := testString + dm.dsCourierOrder_List.DataSet.FieldByName('POSITION_NAME').Text
-//        + ' ' + dm.dsCourierOrder_List.DataSet.FieldByName('PRICE').Text  + #13#10;
-//        dm.dsCourierOrder_List.DataSet.Next;
-//      end;
-//        jsonArrayElement := tJsonObject.Create;
-//        jsonArrayElement
-//          .addPair('id', dm.dsCourierOrder.DataSet.FieldByName('ID').Value)
-//          .addPair('address', dm.dsCourierOrder.DataSet.FieldByName('ADDRESS').Value)
-//          .addPair('start_time', DateTimeToStr(dm.dsCourierOrder.DataSet.FieldByName('START_TIME').Value))
-//          .addPair('name', dm.dsCourierOrder.DataSet.FieldByName('NAME').Value)
-//          .addPair('phone', dm.dsCourierOrder.DataSet.FieldByName('PHONE_NUMBER').Value)
-//          .addPair('is_reported', dm.dsCourierOrder.DataSet.FieldByName('IS_REPORTED').Value)
-//          .addPair('content', testString);
-      jsonToSend.AddPair('result', jsonArrayElement);
-      stringToSend := jsonToSend.ToString;
-      for i := 0 to stringToSend.Length do
-        begin
-          outputByteArray[2*i] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[0];
-          outputByteArray[2*i+1] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[1];
-        end;
-      Socket.SendBuf(outputbyteArray, jsonToSend.ToString.Length*2);
-    end;
-    if operation = 'orderReport' then
-     begin
-       jsonToSend := tJsonObject.Create;
-       jsonToSend.AddPair('result', dm.SetReport(
-          StrToInt(getJsonStringAttribute(receivedJson, 'id'))
-        ));
-       jsonToSend.AddPair('type', 'orderCourier');
-
-       jsonToSend.AddPair('orders', jsonArrayElement);
-       stringToSend := jsonToSend.ToString;
-       for i := 0 to stringToSend.Length do
-        begin
-          outputByteArray[2*i] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[0];
-          outputByteArray[2*i+1] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[1];
-        end;
-        UpdateDB;
-      Socket.SendBuf(outputbyteArray, jsonToSend.ToString.Length*2);
-     end;
-   //Конец добавления
+//  if operation = 'courierOrders' then
+//    begin
+//      courierId := getJsonStringAttribute(receivedJson, 'login').ToInteger();
+//      jsonArray := tJsonArray.Create;
+//      jsonToSend := tJsonObject.Create;
+//      jsonToSend.AddPair('type', 'courierOrders');
+////      dm.qCourierOrders.Close;
+////      dm.qCourierOrders.ParamByName('in_id').Value := courierId;
+////      dm.qCourierOrders.Open;
+////      testString := dm.qCourierOrders.Text;
+////      dm.dsCourierOrders.DataSet := dm.qCourierOrders;
+////      while not dm.dsCourierOrders.DataSet.Eof do
+////      begin
+////        jsonArray.AddElement(tJsonObject.Create);
+////        jsonArrayElement := jsonArray.Items[pred(jsonArray.Count)] as tJsonObject;
+////        jsonArrayElement
+////          .addPair('id', dm.dsCourierOrders.DataSet.FieldByName('id').Value)
+////          .addPair('address', dm.dsCourierOrders.DataSet.FieldByName('address').Value)
+////          .addPair('start_time', DateTimeToStr(dm.dsCourierOrders.DataSet.FieldByName('start_time').Value));
+////        dm.dsCourierOrders.DataSet.Next;
+////      end;
+//      jsonToSend.AddPair('result', jsonArray);
+//      stringToSend := jsonToSend.ToString;
+//      for i := 0 to stringToSend.Length do
+//        begin
+//           outputByteArray[2*i] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[0];
+//          outputByteArray[2*i+1] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[1];
+//        end;
+//      Socket.SendBuf(outputbyteArray, jsonToSend.ToString.Length*2);
+//    end;
+//    //Начало добавления
+//  if operation = 'courierOrder' then
+//    begin
+//      orderId := getJsonStringAttribute(receivedJson, 'id').ToInteger();
+//      jsonToSend := tJsonObject.Create;
+//      jsonToSend.AddPair('type', 'courierOrder');
+////      dm.qCourierOrder.Close;
+////      dm.qCourierOrder.ParamByName('in_order_id').Value := orderId;
+////      dm.qCourierOrder.Open;
+////      testString := '';
+////      dm.dsCourierOrder.DataSet := dm.qCourierOrder;
+////      dm.qCourierOrder_List.Close;
+////      dm.qCourierOrder_List.ParamByName('in_order_id').Value := dm.dsCourierOrder.DataSet.FieldByName('ID').Value;
+////      dm.qCourierOrder_List.Open;
+////      dm.dsCourierOrder_List.DataSet := dm.qCourierOrder_List;
+////      while not dm.dsCourierOrder_List.DataSet.Eof do
+////      begin
+////        testString := testString + dm.dsCourierOrder_List.DataSet.FieldByName('POSITION_NAME').Text
+////        + ' ' + dm.dsCourierOrder_List.DataSet.FieldByName('PRICE').Text  + #13#10;
+////        dm.dsCourierOrder_List.DataSet.Next;
+////      end;
+////        jsonArrayElement := tJsonObject.Create;
+////        jsonArrayElement
+////          .addPair('id', dm.dsCourierOrder.DataSet.FieldByName('ID').Value)
+////          .addPair('address', dm.dsCourierOrder.DataSet.FieldByName('ADDRESS').Value)
+////          .addPair('start_time', DateTimeToStr(dm.dsCourierOrder.DataSet.FieldByName('START_TIME').Value))
+////          .addPair('name', dm.dsCourierOrder.DataSet.FieldByName('NAME').Value)
+////          .addPair('phone', dm.dsCourierOrder.DataSet.FieldByName('PHONE_NUMBER').Value)
+////          .addPair('is_reported', dm.dsCourierOrder.DataSet.FieldByName('IS_REPORTED').Value)
+////          .addPair('content', testString);
+//      jsonToSend.AddPair('result', jsonArrayElement);
+//      stringToSend := jsonToSend.ToString;
+//      for i := 0 to stringToSend.Length do
+//        begin
+//          outputByteArray[2*i] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[0];
+//          outputByteArray[2*i+1] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[1];
+//        end;
+//      Socket.SendBuf(outputbyteArray, jsonToSend.ToString.Length*2);
+//    end;
+//    if operation = 'orderReport' then
+//     begin
+//       jsonToSend := tJsonObject.Create;
+//       jsonToSend.AddPair('result', dm.SetReport(
+//          StrToInt(getJsonStringAttribute(receivedJson, 'id'))
+//        ));
+//       jsonToSend.AddPair('type', 'orderCourier');
+//
+//       jsonToSend.AddPair('orders', jsonArrayElement);
+//       stringToSend := jsonToSend.ToString;
+//       for i := 0 to stringToSend.Length do
+//        begin
+//          outputByteArray[2*i] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[0];
+//          outputByteArray[2*i+1] := TEncoding.UTF8.GetBytes(stringToSend[i + 1])[1];
+//        end;
+//        UpdateDB;
+//      Socket.SendBuf(outputbyteArray, jsonToSend.ToString.Length*2);
+//     end;
+//   //Конец добавления
 end;
 
 end.
